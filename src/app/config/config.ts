1cdb07fe551e471cb96d43ffdd75a12e038c51ac
carlos andres fajardo pedraza <carlosafp84@hotmail.com>
2020-07-18 23:00:24 -0500

    Ajustes migracion sitio

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,3 @@
-export const URL_SERVICIOS = 'https://localhost:44352';
-//export const URL_SERVICIOS = 'http://cafajardop-001-site1.itempurl.com'
+//export const URL_SERVICIOS = 'https://localhost:44352';
+//export const URL_SERVICIOS =   'http://cafajardop-001-site1.itempurl.com'
+export const URL_SERVICIOS = 'http://cnrinnova-001-site2.ctempurl.com'
